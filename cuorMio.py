2e8d5ce43037c51a74870a16ef07d5adf409a90c
Diego Simoncini <simoncinidiego10@gmail.com>
2025-05-05 11:28:10 +0200

    d

--- cuorMio.py
+++ cuorMio.py
@@ -259,7 +259,7 @@ def run_script():
 def check_run_script():
     now_rome = datetime.datetime.now(ZoneInfo('Europe/Rome'))
     # Venerdì -> weekday()==4
-    if now_rome.weekday() == 4 and now_rome.hour == 10 and now_rome.minute == 3:
+    if now_rome.weekday() == 0 and now_rome.hour == 11 and now_rome.minute == 35:
         print("[SCHED] Eseguo run_script() alle 6:00 di venerdì", flush=True)
         run_script()
     else:
